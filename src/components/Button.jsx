e812922a9cebd1314c85f866ee64bc61926008ce
Akhil Royal <akhilroyal5187@gmail.com>
2025-08-04 19:16:02 +0530

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+// import './App.css';
+
+function Button() {
+  const fonts = ['font-sans', 'font-serif', 'font-bold'];
+  const colours = ['bg-red-800', 'bg-blue-800', 'bg-green-800'];
+
+  
+  const [selectedFont, setSelectedFont] = useState('font-sans');
+  const [colour , setColour] = useState('bg-gray-950');  
+  const [toggleFont, setToggleFont] = useState('font-sans');
+  const [toggleColour, setToggleColour] = useState('bg-gray-950');
+
+
+
+  const handleToggle = () => {
+    setToggleFont((prev) => (prev === 'font-sans' ? 'font-serif' : 'font-sans'));
+    setToggleColour((prev) => (prev === 'bg-gray-950' ? 'bg-gray-600' : 'bg-gray-950'));
+  };
+
+
+
+  const handleFontChange = (e) => {
+    setToggleColour(e.target.value);
+    // setColour(e.target.value);
+    setSelectedFont(e.target.value);
+  };
+
+
+  return (
+    <div className={`card  flex gap-10 p-10 flex-col rounded-2xl m-auto w-100   ${colour} `}>
+      
+
+      <div
+        onClick={handleToggle}
+        className={`${toggleColour} rounded border-2 py-2`}
+      >
+        <span className={`${toggleFont} ${toggleColour} py-1 ml-6 `}>Toggle  Button</span>
+      </div>
+
+     <select
+        value={colour}
+        onChange={(e)=>setColour(e.target.value)}
+        className="px-3 py-2 border rounded "
+      >
+        {colours.map((c) => (
+          <option key={c} value={c} className='bg-gray-950'>
+            {c}
+          </option>
+        ))}
+      </select>
+
+      <select 
+      value = {selectedFont}
+      onChange={handleFontChange}
+      className='px-3 py-2 border rounded'
+      >
+        {fonts.map((f)=>(<option key={f} value={f} className='bg-gray-950'>
+            {f}
+            </option>))}
+          
+            
+
+      </select>
+
+      
+    </div>
+  );
+}
+
+export default Button;
